3d056682b59e65e8057555959b26d62e2e9db7d3
carraes <carraeshb@gmail.com>
2024-03-29 19:20:27 -0300

    feat: create db factory

---
+++ src/factories/get_db.py
@@ -0,0 +1,12 @@
+from typing import Iterator
+from pymongo import MongoClient
+
+from models.db import MongoDatabase
+
+
+def get_database() -> Iterator[MongoDatabase]:
+    client = MongoClient("mongodb://user:password@localhost:27017/")
+    try:
+        yield MongoDatabase(client["cazador"])
+    finally:
+        client.close()
