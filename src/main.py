36979eef52c9ff199e21d191d7fd3a15c45b93fa
carraes <carraeshb@gmail.com>
2024-03-29 23:57:40 -0300

    feat: start controllers

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from factories.get_db import get_database
-from models.listing import TestList
 import uvicorn
 from fastapi import Depends, FastAPI
 from pymongo.database import Database
@@ -7,11 +6,8 @@ from pymongo.database import Database
 app = FastAPI()
 
 
-@app.get("/")
+@app.post("/update")
 def test_db(db: Database = Depends(get_database)) -> dict[str, str]:
-    test_list = TestList(name="John Doe", age=25)
-    result = db.collection(TestList).insert_one(test_list.model_dump())
-
     return {"message": "Hello World"}
 
 
