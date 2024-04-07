54162b2122edf468b8526ace0cc5460e0162b513
carraes <carraeshb@gmail.com>
2024-04-06 21:03:42 -0300

    fix: some changes for docker

--- src/factories/db.py
+++ src/factories/db.py
@@ -5,7 +5,7 @@ from adapters.db import MongoDatabase
 
 
 def get_database() -> Iterator[MongoDatabase]:
-    client = MongoClient("mongodb://user:password@localhost:27017/")
+    client = MongoClient("mongodb://user:password@mongo:27017/")
     try:
         yield MongoDatabase(client["cazador"])
     finally:
