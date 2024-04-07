9480c41a574e9aeab0106e2b930ec883b2183c0f
carraes <carraeshb@gmail.com>
2024-04-07 03:21:06 -0300

    feat: add promethues client

--- src/main.py
+++ src/main.py
@@ -1,11 +1,31 @@
+from typing import Awaitable, Callable
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request, Response
 
 import services
 import models
 from factories.listing import get_listing_service
+from prometheus_client import CollectorRegistry, Counter, make_asgi_app
 
 app = FastAPI()
+
+custom_registry = CollectorRegistry()
+
+requests_total = Counter(
+    "request_count", "Total request count", registry=custom_registry
+)
+
+metrics_app = make_asgi_app(registry=custom_registry)
+app.mount("/metrics", metrics_app)
+
+
+@app.middleware("http")
+async def count_requests(
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
+    response = await call_next(request)
+    requests_total.inc()
+    return response
 
 
 @app.get("/healthz")
